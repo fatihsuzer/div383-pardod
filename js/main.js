2a37b8985498b77b367f85f9359a1aeda5768f2a
fatihsuzer <maclabam@gmail.com>
2021-06-08 03:01:40 +0300

    Revert "Development search"

--- js/main.js
+++ js/main.js
@@ -3,22 +3,3 @@ document
   .addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("menu-show");
   });
-
-const swiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  loop: true,
-  centeredSlides: true,
-  centeredSlidesBounds: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  slidesPerView: 1,
-  spaceBetween: 20,
-});
--- js/path.js
+++
@@ -1,9 +0,0 @@
-module.exports = function pathway() {
-  console.log("It's working! :D");
-  var path = window.location.pathname;
-  if (path === "/search") {
-    return "include headersigned";
-  } else {
-    return "include header";
-  }
-};
